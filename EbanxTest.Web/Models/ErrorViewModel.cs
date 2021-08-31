c9c17bb7d3ca62c00a22900e1e78f22012849eaf
JVSCARANO <44774552+JVSCARANO@users.noreply.github.com>
2021-08-31 16:36:36 -0300

    Add files via upload

---
+++ EbanxTest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EbanxTest.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
