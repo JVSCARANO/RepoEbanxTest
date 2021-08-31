40bcde37a67bd31155e6b6b2ee6ce3d6f7e49fa0
JVSCARANO <44774552+JVSCARANO@users.noreply.github.com>
2021-08-31 16:35:44 -0300

    Add files via upload

---
+++ EbanxTest.Web.API/Models/Balance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EbanxTest.Web.API.Models
+{
+    public class Balance
+    {
+        [Key]
+        private int BalanceID { get; set; }
+        [Column (TypeName = "double")]
+        private double BalanceValue { get; set; }
+    }
+}
